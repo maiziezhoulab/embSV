3b3b8136cb344c94ed1b672e7b6384f6c45961b8
volcano1998 <54039319+volcano1998@users.noreply.github.com>
2022-05-15 09:27:39 -0500

    Add files via upload

---
+++ bin/LSHVec/scripts/fastseq/hashSeq.py
@@ -0,0 +1,158 @@
+
+#!/usr/bin/env python
+ 
+import sys, getopt, os
+import multiprocessing
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+import csparc as kg 
+from joblib import Parallel, delayed
+from rand_proj import RandProj
+from tqdm import tqdm
+import utils 
+
+logger = utils.get_logger("hashSeq")
+
+ONE_HOT_MAP = {'A':'00', 'C':'01', 'G':'10', 'T':'11'}
+
+
+def one_hot_hash(kmer):
+    kmer = "".join([ONE_HOT_MAP[u] for u in kmer])
+    return int(kmer, 2)
+
+
+def gen_for_line_fnv(line, k, bucket):
+    seq = line.split("\t")[2].strip()
+    return [kg.fnv_hash(u) % bucket for u in kg.generate_kmer_for_fastseq(seq, k)]
+
+
+def gen_for_line_onehot(line, k):
+    seq = line.split("\t")[2].strip()
+    return [one_hot_hash(u) for u in kg.generate_kmer_for_fastseq(seq, k)]
+
+
+def gen_for_line_lsh(line, k, rp, bucket):
+    seq = line.split("\t")[2].strip()
+    return [u % bucket for u in rp.hash_read(seq)[0]]
+
+
+def convert(in_file, out_file, hash_fun, kmer_size, n_thread, hash_size, batch_size, bucket, create_lsh_only, lsh_file):
+    logger.info("start converting...")
+
+    def f(lines, fout, rp=None):
+        if hash_fun == 'fnv':
+            sequences = Parallel(n_jobs=n_thread)(delayed(gen_for_line_fnv)(line, kmer_size, bucket) for line in tqdm(lines))
+        elif hash_fun == 'onehot':
+            sequences = Parallel(n_jobs=n_thread)(delayed(gen_for_line_onehot)(line, kmer_size) for line in tqdm(lines))
+        else:
+            sequences = Parallel(n_jobs=n_thread)(delayed(gen_for_line_lsh)(line, kmer_size, rp, bucket) for line in tqdm(lines))
+        
+        for u in sequences:
+            fout.write(" ".join([str(v) for v in u]))
+            fout.write("\n")
+    
+    logger.info ("parameters: " + str(locals()))
+    rp = None
+    if hash_fun == 'lsh':
+        if lsh_file:
+            logger.info("load lsh from file " + lsh_file)
+            rp = pickle.load(open(lsh_file, 'rb'))
+        else:
+            logger.info("creating hash ...")        
+            with open(in_file) as fp:
+                lines = list(fp)
+            rp = RandProj(kmer_size=kmer_size, hash_size=hash_size, n_thread=n_thread)
+            rp.create_hash(lines)
+            pickle.dump(rp, open(out_file + ".lsh.pkl", 'wb'))
+            logger.info("finish creating hash ...")
+            if create_lsh_only: return 
+            
+    cnt = 0
+    with open(out_file, 'wt') as fout:
+        with open(in_file) as fp:
+            lines = []
+            for line in fp:
+                lines.append(line)
+                if len(lines) >= batch_size:
+                    f(lines, fout, rp)
+                    cnt += len(lines)
+                    lines = []
+                    logger.info ("written {} lines".format(cnt))
+
+        if len(lines) > 0:
+            f(lines, fout, rp)
+            cnt += len(lines)
+            lines = []
+            logger.info ("written {} lines".format(cnt))            
+    
+    logger.info("finish converting...")
+
+    
+def main(argv):
+    in_file = ''
+    hash_fun = ""
+    out_file = ''
+    lsh_file = ""
+    create_lsh_only = False
+    kmer_size = 31
+    hash_size = 22
+    batch_size = 100000
+    bucket = 20000000
+    n_thread = max(1, multiprocessing.cpu_count() - 1)
+    
+    help_msg = sys.argv[0] + ' -i <seq_file> --hash <fnv or lsh> -o <outfile> [-k <kmer_size>] [--n_thread <n>] [--hash_size <m>] [--batch_size <n>] [--bucket <n>] [--lsh_file <file>] [--create_lsh_only]'
+    if len(argv) < 2:
+        print (help_msg)
+        sys.exit(2) 
+    else:
+        try:
+            opts, args = getopt.getopt(argv, "hi:o:k:", ["in_file=", "hash=", "out_file=", "n_thread=", "hash_size=", 'batch_size=', 'bucket=', 'create_lsh_only', 'lsh_file='])
+        except getopt.GetoptError:
+            print (help_msg)
+            sys.exit(2)
+        for opt, arg in opts:
+            if opt == '-h':
+                print (help_msg)
+                sys.exit()
+            elif opt in ("-i", "--in_file"):
+                in_file = arg
+            elif opt in ("-k"):
+                kmer_size = int(arg)
+                assert kmer_size > 1                
+            elif opt in ("--n_thread"):
+                n_thread = int(arg)
+                assert n_thread > 0               
+            elif opt in ("--hash"):
+                hash_fun = arg
+            elif opt in ("--hash_size"):
+                hash_size = int(arg)
+                assert hash_size > 0
+            elif opt in ("--lsh_file"):
+                lsh_file = arg                
+            elif opt == '--create_lsh_only':
+                create_lsh_only = True
+            elif opt in ("--batch_size"):
+                batch_size = int(arg)
+                assert batch_size > 0 
+            elif opt in ("--bucket"):
+                bucket = int(arg)
+                assert bucket > 0                                                 
+            elif opt in ("-o", "--out_file"):
+                out_file = arg
+        if hash_fun not in ['fnv', 'lsh', 'onehot']:
+            print (help_msg)
+            exit(3)  
+        if hash_fun == 'lsh' and create_lsh_only and lsh_file:
+            print ('lsh_file and create_lsh_only cannot both be specified')
+            print (help_msg)
+            exit(4)  
+        assert os.path.exists(in_file), infile + " does not exists"
+        if lsh_file:
+            assert os.path.exists(lsh_file), lsh_file + " does not exists"
+    convert(in_file, out_file, hash_fun, kmer_size, n_thread=n_thread, hash_size=hash_size, batch_size=batch_size, bucket=bucket, create_lsh_only=create_lsh_only, lsh_file=lsh_file)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ bin/LSHVec/scripts/misc/run_hmp_clf.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python
+ 
+import sys, getopt
+import os , multiprocessing
+import logging
+import numpy as np 
+import pandas as pd 
+
+
+_LOGGERS = {}
+
+
+def get_logger(name):
+    if name not in _LOGGERS:
+        # create logger
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.INFO)
+        
+        # create console handler and set level to debug
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        
+        # create formatter
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        
+        # add formatter to ch
+        ch.setFormatter(formatter)
+        
+        # add ch to logger
+        logger.addHandler(ch)
+        _LOGGERS[name] = logger 
+    return _LOGGERS[name]
+
+logger = get_logger("run_hmp_clf")
+
+
+
+def create_dir_if_not_exists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def shell_run_and_wait(command, working_dir=None, env=None):
+    logger.info("running "+command)
+    curr_dir = os.getcwd()
+    if working_dir is not None:
+        os.chdir(working_dir)
+    command = command.split(" ")
+    import subprocess
+    
+    process = subprocess.Popen(command, env=env)
+    process.wait()
+    if working_dir is not None:
+        os.chdir(curr_dir)
+    return process.returncode
+
+def shell_run_and_wait2(command1, command2, working_dir=None, env=None):
+    logger.info("running {} | {}".format(command1,command2))
+    curr_dir = os.getcwd()
+    if working_dir is not None:
+        os.chdir(working_dir)
+    import subprocess
+    ps = subprocess.Popen( command1.split(" "), stdout=subprocess.PIPE)
+    output = subprocess.check_output(command2.split(" "), stdin=ps.stdout)
+    ps.wait()
+    if working_dir is not None:
+        os.chdir(curr_dir)
+    return ps.returncode
+
+
+def make_train_test(hashfile, labels, n):
+    with open('data.train', 'wt') as ftrain, open('data.test', 'wt') as ftest:
+        with open(hashfile) as fin:
+            for i, line in enumerate(fin):
+                if i < n:
+                    ftrain.write("__label__" + labels[i] + " " + line)
+                else:
+                    ftest.write("__label__" + labels[i] + " " + line)
+
+
+def create_labels(seqfile, label_type):
+    s = """
+286    Pseudomonas    genus    Proteobacteria    Gammaproteobacteria
+357    Agrobacterium    genus    Proteobacteria    Alphaproteobacteria
+475    Moraxella    genus    Proteobacteria    Gammaproteobacteria
+481    Neisseriaceae    family    Proteobacteria    Betaproteobacteria
+543    Enterobacteriaceae    family    Proteobacteria    Gammaproteobacteria
+570    Klebsiella    genus    Proteobacteria    Gammaproteobacteria
+1279    Staphylococcus    genus    Firmicutes    Bacilli
+1301    Streptococcus    genus    Firmicutes    Bacilli
+1386    Bacillus    genus    Firmicutes    Bacilli
+1653    Corynebacteriaceae    family    Actinobacteria    Actinobacteria
+1716    Corynebacterium    genus    Actinobacteria    Actinobacteria
+2070    Pseudonocardiaceae    family    Actinobacteria    Actinobacteria
+13687    Sphingomonas    genus    Proteobacteria    Alphaproteobacteria
+41294    Bradyrhizobiaceae    family    Proteobacteria    Alphaproteobacteria
+80864    Comamonadaceae    family    Proteobacteria    Betaproteobacteria
+85031    Nakamurellaceae    family    Actinobacteria    Actinobacteria
+"""
+    s = [u.strip() for u in s.split("\n") if u.strip()]
+    s = [u.split("\t") for u in s]
+    s = pd.DataFrame(s)
+
+    labels = pd.read_csv(seqfile, usecols=[1], sep='\t', header=None)
+    
+    if label_type == 'org':
+        labels = labels[1].map(lambda u: u.split("-")[1]).values
+    else:
+        labels = labels[1].map(lambda u: u.split("-")[2]).values
+
+    if label_type == 'phylum':        
+        m = s.set_index(0)[3].to_dict()
+        labels = np.array([m[u] for u in labels])
+    elif label_type == 'class':
+        m = s.set_index(0)[4].to_dict()
+        labels = np.array([m[u] for u in labels])
+        
+    logger.info("Has {} labels".format(len(set(labels))))
+    logger.info("Labels: " + " ".join(set(labels)))
+    return labels    
+
+
+def copy_file(src, dest):
+    if src.endswith('.zst'):
+        cmd1 = "cat {}".format(src)
+        cmd2 = "zstd -dfo {}".format(dest)
+        status = shell_run_and_wait2(cmd1,cmd2)
+    else:
+        cmd = "cp {} {}".format(src, dest)
+        status = shell_run_and_wait(cmd)
+    assert status == 0                
+
+                    
+def run(in_file, hash_file, label_type, out_prefix, working_dir, wordNgrams, word_dim, learning_rate, epoch, loss_fun, n_thread):
+    logger.info("start running ...")
+    logger.info ("parameters: " + str(locals()))
+
+    in_file = os.path.abspath(in_file)
+    logger.info("use seq file: " + in_file)
+    assert  os.path.exists(in_file)
+
+    hash_file = os.path.abspath(hash_file)
+    logger.info("use seq file: " + hash_file)
+    assert  os.path.exists(hash_file)
+    
+    if os.path.exists(working_dir): logger.warn("working dir <{}> exists. will overide.".format(working_dir))
+    create_dir_if_not_exists(working_dir)
+    os.chdir(working_dir)
+    logger.info("working dir: " + os.getcwd())
+    
+    copy_file(in_file, "data.seq")
+    copy_file(hash_file, "data.hash")
+    logger.info("finish copying files")
+    
+    labels = create_labels('data.seq', label_type)
+    
+    make_train_test('data.hash', labels, n=450000)
+    logger.info("finish making train test")
+    
+    cmd = "fastseq supervised -input data.train -output model -lr {} -epoch {} -wordNgrams {} -dim {} -loss {} -thread {}".format(
+                     learning_rate, epoch, wordNgrams, word_dim, loss_fun, n_thread)
+    status = shell_run_and_wait(cmd)
+    logger.info("finish running train")
+    assert status == 0
+    
+    cmd = "fastseq test model.bin data.test" 
+    status = shell_run_and_wait(cmd)
+    logger.info("finish running test")
+    assert status == 0
+    
+    logger.info("finish running...")
+
+    
+def main(argv):
+    in_file = ''
+    label_type = ""
+    loss_fun = "hs"
+    out_prefix = 'model'
+    hash_file = ''
+    working_dir = "."
+    learning_rate = 0.5
+    epoch = 5
+    wordNgrams = 1
+    word_dim = 50
+    n_thread = max(1, multiprocessing.cpu_count() - 1)
+    
+    help_msg = sys.argv[0] + ' -i <seq_file> --hash <hash_file> --dim <dim> --label <label>  [--lr <learning rate>] [--n_thread <n>] [--epoch <n>] [--wordNgrams <n>] [--loss <loss>] [--working_dir <working_dir>]'
+    if len(argv) < 2:
+        print help_msg
+        sys.exit(2) 
+    else:
+        try:
+            opts, args = getopt.getopt(argv, "hi:", ["in_file=", "lr=", "dim=", "n_thread=", "epoch=", 'wordNgrams=', 'loss=', 'working_dir=', 'hash=', 'label='])
+        except getopt.GetoptError as e:
+            print e
+            print help_msg
+            sys.exit(2)
+        for opt, arg in opts:
+            if opt == '-h':
+                print help_msg
+                sys.exit()
+            elif opt in ("-i", "--in_file"):
+                in_file = arg
+            elif opt in ("--loss"):
+                loss_fun = arg
+            elif opt in ("--label"):
+                label_type = arg
+                assert label_type in [ 'org', 'tax', 'class', 'phylum']                
+            elif opt in ("--hash"):
+                hash_file = arg                
+            elif opt in ("--working_dir"):
+                working_dir = arg                                                
+            elif opt in ("--n_thread"):
+                n_thread = int(arg)
+                assert n_thread > 0               
+            elif opt in ("--lr"):
+                learning_rate = float(arg)
+                assert learning_rate > 0
+            elif opt in ("--epoch"):
+                epoch = int(arg)
+                assert epoch > 0
+            elif opt in ("--dim"):
+                word_dim = int(arg)
+                assert word_dim > 0                
+            elif opt in ("--wordNgrams"):
+                wordNgrams = int(arg)
+                assert wordNgrams > 0
+                                
+    run(in_file, hash_file, out_prefix, label_type, working_dir=working_dir, wordNgrams=wordNgrams, word_dim=word_dim, learning_rate=learning_rate, epoch=epoch, loss_fun=loss_fun, n_thread=n_thread)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
